77e88e76cfc343e4437b4ed929f30883ff644f25
thiswhitenike@outlook.com <thiswhitenike@outlook.com>
2022-02-06 20:31:42 +0700

    готово для 4 линий

--- Sparam/SParameters/SParams.cs
+++ Sparam/SParameters/SParams.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using MathNet.Numerics;
+using System.Numerics;
 using MathNet.Numerics.LinearAlgebra;
 
 namespace SParameters
@@ -91,7 +91,7 @@ namespace SParameters
         /// <summary>
         /// Мнимая единица.
         /// </summary>
-        public Complex32 Zi = new Complex32(0, 1);
+        public Complex Zi = new Complex(0, 1);
 
         /// <summary>
         /// Расчёт S-параметров.
@@ -140,12 +140,12 @@ namespace SParameters
                     ((double)Nf - 1) * i;
                 w[i] = 2 * Math.PI * f[i];
             }
-            
+
             var um = eigen.EigenVectors;
-            var dz = Matrix<Complex32>.Build.Dense(4, 4);
+            var dz = Matrix<Complex>.Build.Dense(4, 4);
             for (var i = 0; i < Z.Length; i++)
             {
-                dz[i, i] = (Complex32)Math.Sqrt(Z[i]);
+                dz[i, i] = Math.Sqrt(Z[i]);
             }
             var v = Matrix<double>.Build.Dense(4, 4);
             var vivec = Vector<double>.Build.Dense(4);
@@ -156,39 +156,37 @@ namespace SParameters
             }
 
             var im = dc * um * v * 0.001;
-            var co = new List<Matrix<Complex32>>();
-            var sc = new List<Matrix<Complex32>>();
+            var co = new List<Matrix<Complex>>();
+            var sc = new List<Matrix<Complex>>();
 
             for (var i = 0; i < Nf; i++)
             {
-                var tempM = Matrix<Complex32>.Build.Dense(4, 4);
+                var tempM = Matrix<Complex>.Build.Dense(4, 4);
                 var tempV = (w[i] / vivec) * Len;
 
                 for (var j = 0; j < v.ColumnCount; j++)
                 {
-                    tempM[j, j] = 1 / Complex32.Tanh(
-                        (Complex32)tempV[j] * Zi);
+                    tempM[j, j] = 1 / Complex.Tanh(tempV[j] * Zi);
                 }
                 co.Add(tempM);
-                var tet = Matrix<Complex32>.Build.Dense(4, 4);
+                var tet = Matrix<Complex>.Build.Dense(4, 4);
                 for (var j = 0; j < v.ColumnCount; j++)
                 {
-                    tet[j, j] = 1 / Complex32.Sinh(
-                        (Complex32)tempV[j] * Zi);
+                    tet[j, j] = 1 / Complex.Sinh(tempV[j] * Zi);
                 }
                 sc.Add(tet);
             }
 
-            var yaa = new List<Matrix<Complex32>>();
-            var yab = new List<Matrix<Complex32>>();
-            var imComplex = Matrix<Complex32>.Build.Dense(4, 4);
-            var umComplex = Matrix<Complex32>.Build.Dense(4, 4);
+            var yaa = new List<Matrix<Complex>>();
+            var yab = new List<Matrix<Complex>>();
+            var imComplex = Matrix<Complex>.Build.Dense(4, 4);
+            var umComplex = Matrix<Complex>.Build.Dense(4, 4);
             for (var i = 0; i < im.ColumnCount; i++)
             {
                 for (var j = 0; j < im.ColumnCount; j++)
                 {
-                    imComplex[i, j] = (Complex32)im[i, j];
-                    umComplex[i, j] = (Complex32)um[i, j];
+                    imComplex[i, j] = im[i, j];
+                    umComplex[i, j] = um[i, j];
                 }
             }
             for (var i = 0; i < Nf; i++)
@@ -197,10 +195,10 @@ namespace SParameters
                 yab.Add(-imComplex * sc[i] * (umComplex.Inverse()));
             }
 
-            var y = new List<Matrix<Complex32>>();
+            var y = new List<Matrix<Complex>>();
             for (var i = 0; i < Nf; i++)
             {
-                var tempY = Matrix<Complex32>.Build.Dense(8, 8);
+                var tempY = Matrix<Complex>.Build.Dense(8, 8);
                 tempY[0, 0] = yaa[i][0, 0];
                 tempY[0, 1] = yaa[i][0, 1];
                 tempY[0, 2] = yaa[i][0, 2];
@@ -280,15 +278,19 @@ namespace SParameters
             }
 
 
-            var ss = new List<Matrix<Complex32>>();
+            var ss = new List<Matrix<Complex>>();
+            var dz1 = Matrix<Complex>.Build.Dense(
+                2, 2, 0);
+            dz1[0, 0] = dz[0, 0];
+            dz1[1, 1] = dz[1, 1];
             switch (mode)
             {
                 case 0:
                     {
-                        var yo = new List<Matrix<Complex32>>();
+                        var yo = new List<Matrix<Complex>>();
                         for (var i = 0; i < Nf; i++)
                         {
-                            var tempM = Matrix<Complex32>.Build.Dense(4, 4);
+                            var tempM = Matrix<Complex>.Build.Dense(4, 4);
                             tempM[0, 0] = y[i][0, 0] + y[i][0, 2] + y[i][2, 0] + y[i][2, 2];
                             tempM[0, 1] = y[i][0, 1] + y[i][0, 3] + y[i][2, 1] + y[i][2, 3];
                             tempM[0, 2] = y[i][0, 4] + y[i][0, 6] + y[i][2, 4] + y[i][2, 6];
@@ -311,14 +313,14 @@ namespace SParameters
                             yo.Add(tempM);
                         }
 
-                        var upsilon = new List<Matrix<Complex32>>();
+                        var upsilon = new List<Matrix<Complex>>();
                         for (var i = 0; i < Nf; i++)
                         {
                             var u = dz * yo[i] * dz;
                             upsilon.Add(u);
                         }
 
-                        var e4 = Matrix<Complex32>.Build.Dense(
+                        var e4 = Matrix<Complex>.Build.Dense(
                                 4, 4, 0);
                         for (var i = 0; i < e4.ColumnCount; i++)
                         {
@@ -333,42 +335,39 @@ namespace SParameters
                     }
                 case 1:
                     {
-                        var meandr = new List<Matrix<Complex32>>();
-                        var yM = new List<Matrix<Complex32>>();
-                        var z = new List<Matrix<Complex32>>();
-                        var zo = new List<Matrix<Complex32>>();
-                        var zi = new List<Matrix<Complex32>>();
-                        var dz1 = Matrix<Complex32>.Build.Dense(
-                            2, 2, 0);
-                        dz1[0, 0] = dz[0, 0];
-                        dz1[1, 1] = dz[1, 1];
-                        var e2 = Matrix<Complex32>.Build.Dense(
+                        var meandr = new List<Matrix<Complex>>();
+                        var z = new List<Matrix<Complex>>();
+                        var zo = new List<Matrix<Complex>>();
+                        var zi = new List<Matrix<Complex>>();
+                        var e2 = Matrix<Complex>.Build.Dense(
                             2, 2, 0);
                         e2[0, 0] = 1;
                         e2[1, 1] = 1;
                         for (var i = 0; i < Nf; i++)
                         {
-                            meandr.Add(Matrix<Complex32>.Build.Dense(8, 8));
-                            yM.Add(Matrix<Complex32>.Build.Dense(8, 8));
-                            z.Add(Matrix<Complex32>.Build.Dense(8, 8));
-                            zo.Add(Matrix<Complex32>.Build.Dense(2, 2));
-                            zi.Add(Matrix<Complex32>.Build.Dense(2, 2));
-                            ss.Add(Matrix<Complex32>.Build.Dense(2, 2));
-                            meandr[i][1, 1] = (Complex32)1e10;
-                            meandr[i][1, 2] = (Complex32)(-1e10);
-                            meandr[i][2, 1] = (Complex32)(-1e10);
-                            meandr[i][2, 2] = (Complex32)1e10;
-                            meandr[i][4, 4] = (Complex32)1e10;
-                            meandr[i][4, 5] = (Complex32)(-1e10);
-                            meandr[i][5, 4] = (Complex32)(-1e10);
-                            meandr[i][5, 5] = (Complex32)1e10;
-                            meandr[i][6, 6] = (Complex32)1e10;
-                            meandr[i][6, 7] = (Complex32)(-1e10);
-                            meandr[i][7, 6] = (Complex32)(-1e10);
-                            meandr[i][7, 7] = (Complex32)1e10;
-                            yM[i] = y[i] + meandr[i];
-                            z[i] = yM[i].Inverse();
-                            Console.WriteLine(z[i]);
+                            meandr.Add(Matrix<Complex>.Build.Dense(
+                                8, 8));
+                            z.Add(Matrix<Complex>.Build.Dense(
+                                8, 8));
+                            zo.Add(Matrix<Complex>.Build.Dense(
+                                2, 2));
+                            zi.Add(Matrix<Complex>.Build.Dense(
+                                2, 2));
+                            ss.Add(Matrix<Complex>.Build.Dense(
+                                2, 2));
+                            meandr[i][1, 1] = 1e10;
+                            meandr[i][1, 2] = (-1e10);
+                            meandr[i][2, 1] = (-1e10);
+                            meandr[i][2, 2] = 1e10;
+                            meandr[i][4, 4] = 1e10;
+                            meandr[i][4, 5] = (-1e10);
+                            meandr[i][5, 4] = (-1e10);
+                            meandr[i][5, 5] = 1e10;
+                            meandr[i][6, 6] = 1e10;
+                            meandr[i][6, 7] = (-1e10);
+                            meandr[i][7, 6] = (-1e10);
+                            meandr[i][7, 7] = 1e10;
+                            z[i] = (y[i] + meandr[i]).Inverse();
                             zo[i][0, 0] = z[i][0, 0];
                             zo[i][0, 1] = z[i][0, 3];
                             zo[i][1, 0] = z[i][3, 0];
@@ -380,13 +379,51 @@ namespace SParameters
                     break;
                 case 2:
                     {
-
+                        var meandr = new List<Matrix<Complex>>();
+                        var z = new List<Matrix<Complex>>();
+                        var yM = new List<Matrix<Complex>>();
+                        var zo = new List<Matrix<Complex>>();
+                        var zi = new List<Matrix<Complex>>();
+                        var e2 = Matrix<Complex>.Build.Dense(
+                            2, 2, 0);
+                        e2[0, 0] = 1;
+                        e2[1, 1] = 1;
+                        for (var i = 0; i < Nf; i++)
+                        {
+                            meandr.Add(Matrix<Complex>.Build.Dense(
+                                8, 8));
+                            yM.Add(Matrix<Complex>.Build.Dense(
+                                8, 8));
+                            z.Add(Matrix<Complex>.Build.Dense(
+                                8, 8));
+                            zo.Add(Matrix<Complex>.Build.Dense(
+                                2, 2));
+                            zi.Add(Matrix<Complex>.Build.Dense(
+                                2, 2));
+                            ss.Add(Matrix<Complex>.Build.Dense(
+                                2, 2));
+                            meandr[i][1, 1] = 1e-10;
+                            meandr[i][2, 2] = 1e10;
+                            meandr[i][3, 3] = 1e-10;
+                            meandr[i][4, 4] = 1e-10;
+                            meandr[i][5, 5] = 1e10;
+                            meandr[i][6, 6] = 1e-10;
+                            yM[i] = y[i] + meandr[i];
+                            z[i] = yM[i].Inverse();
+                            
+                            zo[i][0, 0] = z[i][0, 0];
+                            zo[i][0, 1] = z[i][0, 7];
+                            zo[i][1, 0] = z[i][7, 0];
+                            zo[i][1, 1] = z[i][7, 7];
+                            zi[i] = dz1.Inverse() * zo[i] * dz1.Inverse();
+                            ss[i] = e2 - 2 * (e2 + zi[i]).Inverse();
+                        }
                     }
                     break;
                 default:
                     throw new ArgumentException("Неправильный режим.");
             }
-            
+
             var s11 = new double[Nf];
             var s12 = new double[Nf];
             var s13 = new double[Nf];
@@ -409,20 +446,20 @@ namespace SParameters
             var fi44 = new double[Nf];
             for (var i = 1; i < Nf; i++)
             {
-                s11[i] = 20 * Math.Log10(Complex32.Abs(ss[i][0, 0]));
-                s12[i] = 20 * Math.Log10(Complex32.Abs(ss[i][0, 1]));
-                s22[i] = 20 * Math.Log10(Complex32.Abs(ss[i][1, 1]));
+                s11[i] = 20 * Math.Log10(Complex.Abs(ss[i][0, 0]));
+                s12[i] = 20 * Math.Log10(Complex.Abs(ss[i][0, 1]));
+                s22[i] = 20 * Math.Log10(Complex.Abs(ss[i][1, 1]));
                 fi11[i] = ss[i][0, 0].Phase * 57.3;
                 fi12[i] = ss[i][0, 1].Phase * 57.3;
                 fi22[i] = ss[i][1, 1].Phase * 57.3;
                 if (mode != 0) continue;
-                s13[i] = 20 * Math.Log10(Complex32.Abs(ss[i][0, 2]));
-                s14[i] = 20 * Math.Log10(Complex32.Abs(ss[i][0, 3]));
-                s23[i] = 20 * Math.Log10(Complex32.Abs(ss[i][1, 2]));
-                s24[i] = 20 * Math.Log10(Complex32.Abs(ss[i][1, 3]));
-                s33[i] = 20 * Math.Log10(Complex32.Abs(ss[i][2, 2]));
-                s34[i] = 20 * Math.Log10(Complex32.Abs(ss[i][2, 3]));
-                s44[i] = 20 * Math.Log10(Complex32.Abs(ss[i][3, 3]));
+                s13[i] = 20 * Math.Log10(Complex.Abs(ss[i][0, 2]));
+                s14[i] = 20 * Math.Log10(Complex.Abs(ss[i][0, 3]));
+                s23[i] = 20 * Math.Log10(Complex.Abs(ss[i][1, 2]));
+                s24[i] = 20 * Math.Log10(Complex.Abs(ss[i][1, 3]));
+                s33[i] = 20 * Math.Log10(Complex.Abs(ss[i][2, 2]));
+                s34[i] = 20 * Math.Log10(Complex.Abs(ss[i][2, 3]));
+                s44[i] = 20 * Math.Log10(Complex.Abs(ss[i][3, 3]));
                 fi13[i] = ss[i][0, 2].Phase * 57.3;
                 fi14[i] = ss[i][0, 3].Phase * 57.3;
                 fi23[i] = ss[i][1, 2].Phase * 57.3;
